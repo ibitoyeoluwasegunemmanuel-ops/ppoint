9ca5971e39a9b65a3bc552e20f60a7a9191c7ce8
Ibitoye Oluwasegun Emmanuel <ibitoyeoluwasegunemmanuel-ops@users.noreply.github.com>
2026-03-10 23:14:32 +0100

    Fix Vercel community route imports

--- api/platform/community/addresses/[id]/details.js
+++ api/platform/community/addresses/[id]/details.js
@@ -1,4 +1,4 @@
-import { handleCommunityDetailsUpdate } from '../../../../../_lib/communityAddressApi.mjs';
+import { handleCommunityDetailsUpdate } from '../../../../_lib/communityAddressApi.mjs';
 
 export default async function handler(req, res) {
   return handleCommunityDetailsUpdate(req, res, req.query?.id);
--- api/platform/community/addresses/generate.js
+++ api/platform/community/addresses/generate.js
@@ -1,4 +1,4 @@
-import { handleCommunityGenerate } from '../../../../_lib/communityAddressApi.mjs';
+import { handleCommunityGenerate } from '../../../_lib/communityAddressApi.mjs';
 
 export default async function handler(req, res) {
   return handleCommunityGenerate(req, res);
